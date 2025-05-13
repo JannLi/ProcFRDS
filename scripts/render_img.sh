8adab68acacb52c7280ba7006eaec34a285aba76
LiJian(搜索事业部_搜索研发部) <lijianovo@sogou-inc.com>
2025-05-13 11:24:21 +0800

    modify scripts

--- scripts/render_img.sh
+++
@@ -1,5 +0,0 @@
-#python render_with_predpath.py --blend_path ../test/5_1/out.blend --path_file ../test/5/ego_position_parking.csv --path_spec aaa --out_img_path ../test/5_1/fisheye/
-#python render_with_predpath.py --blend_path ../test/5/out.blend --path_file ../test/5/ego_position_parking.csv --path_spec aaa --out_img_path ../test/5/fisheye/
-python render_with_predpath.py --blend_path ../test/5_2/out.blend --path_file ../test/5/ego_position_parking.csv --path_spec aaa --out_img_path ../test/5_2/fisheye/
-python render_with_predpath.py --blend_path ../test/5_3/out.blend --path_file ../test/5/ego_position_parking.csv --path_spec aaa --out_img_path ../test/5_3/fisheye/
-python render_with_predpath.py --blend_path ../test/5_4/out.blend --path_file ../test/5/ego_position_parking.csv --path_spec aaa --out_img_path ../test/5_4/fisheye/
--- scripts/vlm_data_generator.sh
+++
@@ -1,3 +0,0 @@
-for ((i=2;i<=25;i++));do
-    python vlm_data_generator.py --ego_pose_path ../../lane_marker/vlm/${i}/ego.npy --out_path ../../lane_marker/vlm/${i}/
-done
